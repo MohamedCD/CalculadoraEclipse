aa48069a12303b8b6579c94f943568447aae24b5
Mohamed <mohamed.chahdi.dkikar@gmail.com>
2021-03-07 13:09:28 +0100

    v2 multiplicacion

--- Calculadora/src/Calculadora/Calculadora.java
+++ Calculadora/src/Calculadora/Calculadora.java
@@ -14,11 +14,19 @@ public class Calculadora {
 		return c;
 	}
 	
+	public static int multiplicacion(int a, int b) {
+		int c = a * b;
+		
+		return c;
+	}
+	
 	public static void main(String[] args) {
 		int a = 9;
 		int b = 3;
 		
 		System.out.print("Suma: " + Calculadora.suma(a, b));
 		System.out.print("Resta: " + Calculadora.resta(a, b));
+		System.out.print("Multiplicación: " + Calculadora.multiplicacion(a, b));
+
 	}
 }
