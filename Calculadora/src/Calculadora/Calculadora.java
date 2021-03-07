64d733aeb9ea89f4c8a0a6972521e0fe479420e8
Mohamed <mohamed.chahdi.dkikar@gmail.com>
2021-03-07 12:59:50 +0100

    version 1 (resta)

--- Calculadora/src/Calculadora/Calculadora.java
+++ Calculadora/src/Calculadora/Calculadora.java
@@ -8,10 +8,17 @@ public class Calculadora {
 		return c;
 	}
 	
+	public static int resta(int a, int b) {
+		int c = a - b;
+		
+		return c;
+	}
+	
 	public static void main(String[] args) {
 		int a = 9;
 		int b = 3;
 		
-		System.out.print(Calculadora.suma(a, b));
+		System.out.print("Suma: " + Calculadora.suma(a, b));
+		System.out.print("Resta: " + Calculadora.resta(a, b));
 	}
 }
